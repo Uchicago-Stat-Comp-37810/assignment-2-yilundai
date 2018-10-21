ea1d5cb5ff9c0a0dd0fa3103dfc9cb1391084f8c
Yilun Dai <yilundai@uchicago.edu>
2018-10-21 10:51:20 -0500

    3. change the graphing portion into a function
    change the graphing portion into a function

--- Assignment2_Part1_Yilun_Dai.R
+++ Assignment2_Part1_Yilun_Dai.R
@@ -71,22 +71,31 @@ acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
 
 ### Summary: #######################
 
-par(mfrow = c(2,3))
-hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of a", xlab="True value = red line" )
-abline(v = mean(chain[-(1:burnIn),1]))
-abline(v = trueA, col="red" )
-hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),2]))
-abline(v = trueB, col="red" )
-hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),3]) )
-abline(v = trueSd, col="red" )
-plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a")
-abline(h = trueA, col="red" )
-plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b")
-abline(h = trueB, col="red" )
-plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd")
-abline(h = trueSd, col="red" )
+MCMC_graph <- function(chain, burnIn, trueParam){
+  trueA = trueParam[1]
+  trueB = trueParam[2]
+  trueSd = trueParam[3]
+  par(mfrow = c(2,3))
+  hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of a", xlab="True value = red line" )
+  abline(v = mean(chain[-(1:burnIn),1]))
+  abline(v = trueA, col="red" )
+  hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
+  abline(v = mean(chain[-(1:burnIn),2]))
+  abline(v = trueB, col="red" )
+  hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
+  abline(v = mean(chain[-(1:burnIn),3]) )
+  abline(v = trueSd, col="red" )
+  plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a")
+  abline(h = trueA, col="red" )
+  plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b")
+  abline(h = trueB, col="red" )
+  plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd")
+  abline(h = trueSd, col="red" )
+  return()
+}
+trueParam = c(trueA, trueB, trueSd)
+MCMC_graph(chain, burnIn, trueParam)
+
 
 # for comparison:
 summary(lm(y~x))
