f4d0c2fcf94fd4ddac2f0590474a9ebbf8a185d9
Yilun Dai <yilundai@uchicago.edu>
2018-10-21 22:35:01 -0500

    Assignment2 Part2 ex1 to ex4

---
+++ ex3.py
@@ -0,0 +1,39 @@
+#print a string
+print("I will now count my chickens:")
+
+#print a string and then a number, which is the result of 25 + 30/6
+print("Hens", 25.0 + 30.0 / 6.0)
+#print a string and then a number, which is the result of 100 - 25 * 3 % 4
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+#print a string
+print("Now I will count the eggs:")
+
+#print a number, which is the result of the calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#print a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#print a logical value
+print(3 + 2 < 5 - 7)
+
+#print a string and then a number
+print("What is 3 + 2?", 3 + 2)
+#print a string and then a number
+print("What is 5 - 7?", 5 - 7)
+
+#print a string
+print("Oh, that's why it's False.")
+
+#print a tring
+print("How about some more.")
+
+#print a string and then a logical value
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+#write sth that I need to calculate
+print("6.1 * 8.6 =", 6.1 * 8.6)
+
